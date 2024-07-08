1104d2db7c6772acb06a0f33078c75918fb5b748
tvoisyabr <neperamojny@gmail.com>
2024-07-08 13:13:00 -0700

    Add dynamic grid size feature with user input

--- scripts.js
+++ scripts.js
@@ -1,11 +1,31 @@
 const container = document.querySelector("#container");
+const button = document.querySelector(".btn");
 
-for (let i = 0; i < 16 * 16; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid-item");
+function createGrid(size) {
+    container.innerHTML = "";
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        div.classList.add("grid-item");
 
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "lightblue";
-    });
-    container.appendChild(div);
-}
+        div.style.flex = `1 0 calc(100% / ${size})`;
+        div.style.height = `calc(100% / ${size})`;
+    
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "lightblue";
+        });
+        container.appendChild(div);
+    }
+};
+
+createGrid(16);
+
+button.addEventListener("click", () => {
+    let gridSize = prompt("Enter a number: ");
+
+    while(Number(gridSize) > 100 || gridSize.trim() === "" || isNaN(gridSize)) {
+        gridSize = prompt("Please enter a number less or equal to 100: ");
+    }
+    createGrid(Number(gridSize));
+});
+
+
